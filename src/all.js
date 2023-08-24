63eb2b9ac7ceb8031c7be4e00918607fb3478cf2
lyad <86334337+lyadd@users.noreply.github.com>
2023-08-24 20:41:38 +0200

    Update all.js

--- src/all.js
+++ src/all.js
@@ -1,11 +1,10 @@
 const
     express = require('express'),
-    router = express.Router();
-
-const routes = [
-    { path: '/', view: '1', customMessage: false },
-    { path: '/record', view: '2', customMessage: false }
-];
+    router = express.Router(),
+    routes = [
+        { path: '/', view: '1', customMessage: false },
+        { path: '/record', view: '2', customMessage: false }
+    ];
 
 routes.forEach(route => {
     router.get(route.path, (req, res) => {
